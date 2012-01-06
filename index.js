a670e0746ac42c0b26dd8a50008d7dfcf3e056b2
Michael Krolikowski <mkroli@yahoo.de>
2012-01-06 18:35:51 +0100

    added icons for expanding/collapsing details

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ $(function() {
 		var toggleLink = $(element).children('.togglelink');
 		var description = $(element).children('.description');
 		var link = $(description).children('a.projectlink');
+		var visible = false;
 
+		toggleLink.css('background-image', 'url("plus.png")');
 		link.text("Project's page");
 		description.hide();
 
 		toggleLink.click(function() {
-			description.slideToggle();
+			if (visible) {
+				toggleLink.css('background-image', 'url("plus.png")');
+				description.slideUp();
+			} else {
+				toggleLink.css('background-image', 'url("minus.png")');
+				description.slideDown();
+			}
+			visible = !visible;
 		});
 	});
 });
