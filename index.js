c84c0171aaf101bd0d11ff11773611e5be9feccc
Michael Krolikowski <mkroli@yahoo.de>
2012-01-15 18:48:53 +0100

    added tabs

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
+function getAnchor() {
+	if (document.location.hash.length <= 1)
+		return null;
+	else
+		return document.location.hash.substring(1);
+}
+
 $(function() {
-	$('#content > ul > li').each(function(index, element) {
-		var toggleLink = $(element).children('.togglelink');
-		var description = $(element).children('.description');
-		var link = $(description).children('a.projectlink');
+	/* Menu */
+	var anchor = getAnchor();
+	var currentContent = anchor == null ? 'Projects' : anchor;
+	$('#content > div.content').each(function(index, element) {
+		var id = $(element).attr('id');
+		$('#content > #tabs').append('<li id="' + id + '">' + id + '</li>');
+		if (id != currentContent) {
+			$(element).hide();
+			$('#content > #tabs > li#' + id).addClass('other');
+		} else
+			$('#content > #tabs > li#' + id).addClass('current');
+	});
+	$('#content > #tabs > li').each(
+			function(index, element) {
+				$(element).click(
+						function() {
+							$('#content > #tabs > li').removeClass('current')
+									.addClass('other');
+							$(this).removeClass('other').addClass('current');
+							var newContent = $(this).attr('id')
+							document.location.hash = '#' + newContent;
+							if (currentContent != newContent) {
+								$('#content > #' + currentContent).fadeOut(
+										function() {
+											$('#content > #' + newContent)
+													.fadeIn();
+										});
+								currentContent = newContent;
+							}
+						});
+			});
+
+	/* Description */
+	$('#content > .content > ul > li').each(function(index, element) {
+		var toggleLink = $(element).find('.togglelink');
+		var description = $(element).find('.description');
+		var link = $(description).find('a.projectlink');
 		var visible = false;
 
 		toggleLink.css('background-image', 'url("plus.png")');
